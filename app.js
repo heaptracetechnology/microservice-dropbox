9a945db2e43979058f6b8df1c1d55e5ceca9876a
ht68rohit <rohits@heaptrace.com>
2019-02-13 18:17:13 +0530

    updated service

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var fs = require("fs");
 var app = express();
 app.use(bodyParser.json());
 var UPLOAD_TEMP_FOLDER = './uploads/';
-var ACCESS_TOKEN = 'qSTlcZ-zpyAAAAAAAAAAC1sbJNW8xkPfZGupS6DAoiEBtSqHiuTdrKeD1gVfbaDp';
 
 //Get File List
 app.get('/db-list-files', function (request, response) {
@@ -56,51 +55,6 @@ app.get('/db-get-useraccount', function (request, response) {
 
 
 
-app.post('/db-upload-files', function (request, response) {
-
-  var type = '.txt'
- var tempoutputpath = `${__dirname}\\temp.txt`;
- var ACCESS_TOKEN = process.env.accessToken;
- var ACCESS_TOKEN = 'qSTlcZ-zpyAAAAAAAAAAC1sbJNW8xkPfZGupS6DAoiEBtSqHiuTdrKeD1gVfbaDp';
-  if (ACCESS_TOKEN == undefined) {
-    return response.end("Please provide API Key");
-  }
-  var fpath =`${__dirname}\\my.txt`
-  var Base64filedata = fs.readFileSync(fpath, { encoding: "base64" });
-  // var wrt = fs.writeFile(tempoutputpath, Base64filedata, 'base64', function(err) {
-  //   console.log(err);
-  // });
-  
-     var filedata = fs.readFileSync(tempoutputpath, { encoding: "binary" });
-
-    //  var binary = Buffer.from(Base64filedata,'base64');
-    //  fs.writeFileSync('mydata', buff);
-    //         console.log("mydata : ",buff);
-
-
-// fs.unlink(tempoutputpath, (err) => {
-//   if (err) throw err;
-//   console.log('successfully deleted /tmp/hello');
-// });
-// var filename = process.argv.filename;
-// var filecontent = process.argv.filecontent;
-
-
-
-
-  var dbx = new Dropbox.Dropbox({ accessToken: ACCESS_TOKEN, fetch: fetch });
-  dbx.filesUpload({ path: '/' + 'new11.txt', contents: filedata })
-    .then(function (response) {
-      //var results = document.getElementById('results');
-      //results.appendChild(document.createTextNode('File uploaded!'));
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-
-
-});
 
 app.listen(3000, function () {
   console.log("Working on port 3000");
